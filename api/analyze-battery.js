f999850b1d39fbab1821fdc0f43388a219216f81
guy746 <guy746@gmail.com>
2025-06-26 18:04:48 -0400

    updates
    updates

---
+++ api/analyze-battery.js
@@ -0,0 +1,11 @@
+{
+  "is_battery": true,
+  "object_type": "Lithium Ion",
+  "confidence": "93%",
+  "battery_details": {
+    "voltage": "3.7V",
+    "capacity": "2200mAh",
+    "brand": "LG",
+    ...
+  }
+}
